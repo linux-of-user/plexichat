1036a654002fcd4e258df2ff1c904022f087b550
PlexiChat Automation <autobot@example.com>
2025-09-09 22:02:47 +0100

    Prepare for server start: export get_logging_manager in logging __init__.py to fix import error

--- src/plexichat/core/logging/__init__.py
+++ src/plexichat/core/logging/__init__.py
@@ -1,19 +1,23 @@
 """PlexiChat core logging module - re-exports from unified_logger."""
 
 from .unified_logger import (
-    get_logger,
-    redact_pii,
-    sanitize_for_logging,
     ColoredFormatter,
+    LogCategory,
     StructuredFormatter,
     get_handler_factory,
+    get_logger,
+    get_logging_manager,
+    redact_pii,
+    sanitize_for_logging,
 )
 
 __all__ = [
-    'get_logger',
-    'redact_pii',
-    'sanitize_for_logging',
-    'ColoredFormatter',
-    'StructuredFormatter',
-    'get_handler_factory',
+    "get_logger",
+    "redact_pii",
+    "sanitize_for_logging",
+    "ColoredFormatter",
+    "StructuredFormatter",
+    "get_handler_factory",
+    "LogCategory",
+    "get_logging_manager",
 ]
